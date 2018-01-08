62f2a4b2f082820c9b5642f92a88602f485462b5
Anmol Agarwal <anmol.agarwal2004@yahoo.com>
2018-01-08 20:00:57 +0530

    Initial commit Start

---
+++ ConnectionDetector.java
@@ -0,0 +1,23 @@
+package com.gu.fineanmol.galgotiasuniversitymsim.helper;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ConnectionDetector {
+
+	public static boolean isConnectingToInternet(Context c) {
+		ConnectivityManager connectivity = (ConnectivityManager) c
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (connectivity != null) {
+			NetworkInfo[] info = connectivity.getAllNetworkInfo();
+			if (info != null)
+				for (NetworkInfo anInfo : info)
+					if (anInfo.getState() == NetworkInfo.State.CONNECTED) {
+						return true;
+					}
+
+		}
+		return false;
+	}
+}
